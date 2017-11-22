bf7d170788081ccbf689d513b8132c2df477cec7
Frost <imperio4869@gmail.com>
2017-11-22 11:33:41 +0700

    init project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+import 'semantic-ui-css/semantic.min.css';
+import FixMenu from './fix-menu'
+
+class App extends Component {
+  render() {
+    return (
+      <FixMenu/>
+    );
+  }
+}
+
+export default App;
